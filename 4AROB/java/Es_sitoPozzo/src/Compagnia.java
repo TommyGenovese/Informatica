abbba21899273e709e8390e8f777cfefe1ab7478
TommyGenovese <tommaso.genovese@itiscuneo.eu>
2020-02-21 12:15:51 +0100

    pozzo end

--- 4AROB/java/Es_sitoPozzo/src/Compagnia.java
+++ 4AROB/java/Es_sitoPozzo/src/Compagnia.java
@@ -8,4 +8,17 @@ public class Compagnia {
         this.nome = nome;
     }
 
+    public void addSito(Sito sito){
+        siti.add(sito);
+    }
+
+    @Override
+    public String toString() {
+        String out = "|" + nome + "|\n";
+        for (int i=0; i<siti.size();i++){
+            out+=siti.elementAt(i).toString();
+        }
+        return out;
+    }
+
 }
--- 4AROB/java/Es_sitoPozzo/src/Test.java
+++ 4AROB/java/Es_sitoPozzo/src/Test.java
@@ -1,2 +1,22 @@
 public class Test {
+    public static void main(String[] args) throws ValoreNonValido {
+        Compagnia spacex = new Compagnia("SpaceX");
+        Sito sit = new Sito("Petroliera potente", "america");
+        PozzoMarino pozzo_nel_mare = new PozzoMarino(270.0f,25);
+        PozzoTerrestre pozzo_sulla_terra = new PozzoTerrestre(12, TipoTerreno.ROCCIOSO);
+
+        System.out.println(pozzo_nel_mare.toString());
+        System.out.println(pozzo_sulla_terra.toString());
+
+        sit.addPozzo(pozzo_nel_mare);
+        pozzo_sulla_terra.spegni();
+        sit.addPozzo(pozzo_sulla_terra);
+
+        spacex.addSito(sit);
+
+        System.out.println(spacex.toString());
+
+        System.out.println(sit.greggioAnnuo());
+
+    }
 }
